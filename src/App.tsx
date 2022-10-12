516f8eaff193df441e7df82094995a9d2ea6ee5d
AJ Igherighe <the.coders.dao@gmail.com>
2022-10-12 12:33:04 -0400

    initial app and routing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,69 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
+import { Home } from "./Home";
+import {
+  BrowserRouter,
+  Outlet,
+  Routes,
+  Route,
+  Link as RouteLink,
+} from "react-router-dom";
+
+const Spinner = () => {
+  return (
+    <>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+          <VStack spacing={8}>
+            <Logo h="40vmin" pointerEvents="none" />
+            <Link
+              as={RouteLink}
+              color="teal.500"
+              to="/home"
+              fontSize="2xl"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              To Home
+            </Link>
+          </VStack>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+const pageRoutes = [
+  {
+    page_id: 0,
+    path: "/",
+    element: <Spinner />,
+  },
+  {
+    page_id: 1,
+    path: "/home",
+    element: <Home />,
+  },
+];
 
 export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+  <BrowserRouter>
+    <ChakraProvider theme={theme}>
+      <Outlet />
+      <Routes>
+        {pageRoutes.map((route) => (
+          <Route
+            key={route.page_id}
+            path={route.path}
+            element={route.element}
+          />
+        ))}
+      </Routes>
+    </ChakraProvider>
+  </BrowserRouter>
+);
---
+++ src/Home.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { Logo } from "./Logo";
+
+export const Home = () => <div>Home Page</div>;
